38f0517e6c444d5b976bdd5850553aae103d916f
Lucas Gray <lucascgray@protonmail.com>
2023-03-31 22:59:55 -0700

    added datatypes to relevant arguments in functions

--- antenna.py
+++ antenna.py
@@ -6,10 +6,10 @@ PAUSE = 0.01 # 'clock' speed
 board = pyfirmata.ArduinoMega('/dev/cu.usbmodem101')
 data = pd.read_csv('lookangles.csv')
 
-def bits(angle) -> str:
+def bits(angle: float) -> str:
     return bin(int(angle / 22.5) * 4)[2:].zfill(6)
 
-def get_angles(index) -> str: # select wanted EL and AZ set
+def get_angles(index: int) -> str: # select wanted EL and AZ set
     cmd  = bits(data.J4[index])
     cmd += bits(data.J3[index])
     cmd += bits(data.J2[index])
@@ -28,7 +28,7 @@ def do_zero() -> None: # set all 4 phase shifters to 0
     board.digital[27].write(0)
     time.sleep(PAUSE)
 
-def do_shift(shift) -> None: # phase shifter command function
+def do_shift(shift: str) -> None: # phase shifter command function
     for i in range(0,len(shift)):
         if shift[i] == '1':
             board.digital[23].write(1)
