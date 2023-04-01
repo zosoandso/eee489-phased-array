36e1f45c44d4db358f741b18ca08afef300065ed
Lucas Gray <lucascgray@protonmail.com>
2023-03-31 22:48:18 -0700

    added look angle database and methods for parsing them into command strings

--- antenna.py
+++ antenna.py
@@ -1,40 +1,48 @@
-from pyfirmata import ArduinoMega
-from time import sleep
+import pyfirmata
+import pandas as pd
+import time
 
-PAUSE = 0.01
+PAUSE = 0.01 # 'clock' speed
+board = pyfirmata.ArduinoMega('/dev/cu.usbmodem101')
+data = pd.read_csv('lookangles.csv')
 
-UP = []
-
-board = ArduinoMega('/dev/cu.usbmodem101')
-
-def bits(angle):
+def bits(angle) -> str:
     return bin(int(angle / 22.5) * 4)[2:].zfill(6)
 
-def do_zero(): # set all 4 phase shifters to 0
+def get_angles(index) -> str: # select wanted EL and AZ set
+    cmd  = bits(data.J4[index])
+    cmd += bits(data.J3[index])
+    cmd += bits(data.J2[index])
+    cmd += bits(data.J1[index])
+    return cmd
+
+print(type(get_angles(0)))
+
+def do_zero() -> None: # set all 4 phase shifters to 0
     board.digital[23].write(0)
     for i in range(0,24):
         board.digital[25].write(1)
-        sleep(PAUSE)
+        time.sleep(PAUSE)
         board.digital[25].write(0)
-        sleep(PAUSE)
+        time.sleep(PAUSE)
     board.digital[27].write(1)
-    sleep(PAUSE)
+    time.sleep(PAUSE)
     board.digital[27].write(0)
-    sleep(PAUSE)
+    time.sleep(PAUSE)
 
-def do_shift(shift): # phase shifter command function
+def do_shift(shift) -> None: # phase shifter command function
     for i in range(0,len(shift)):
         if shift[i] == '1':
             board.digital[23].write(1)
-            sleep(PAUSE)
+            time.sleep(PAUSE)
         else:
             board.digital[23].write(0)
-            sleep(PAUSE) 
+            time.sleep(PAUSE) 
         board.digital[25].write(1)
-        sleep(PAUSE)
+        time.sleep(PAUSE)
         board.digital[25].write(0)
-        sleep(PAUSE)
+        time.sleep(PAUSE)
     board.digital[27].write(1)
-    sleep(PAUSE)
+    time.sleep(PAUSE)
     board.digital[27].write(0)
-    sleep(PAUSE)
+    time.sleep(PAUSE)
