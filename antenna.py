93b5467f8a915369caab338a3454f65c8a2c6c36
Lucas Gray <lucascgray@protonmail.com>
2023-04-02 16:43:18 -0700

    added zero to lookangles

--- antenna.py
+++ antenna.py
@@ -3,6 +3,8 @@ import pandas as pd
 import time
 
 PAUSE = 0.01 # 'clock' speed
+ZERO = 0
+
 board = pyfirmata.ArduinoMega('/dev/cu.usbmodem101')
 data = pd.read_csv('lookangles.csv')
 
@@ -15,18 +17,6 @@ def get_angles(index: int) -> str: # select wanted EL and AZ set
     cmd += bits(data.J2[index])
     cmd += bits(data.J1[index])
     return cmd
-
-def do_zero() -> None: # set all 4 phase shifters to 0
-    board.digital[23].write(0)
-    for i in range(0,24):
-        board.digital[25].write(1)
-        time.sleep(PAUSE)
-        board.digital[25].write(0)
-        time.sleep(PAUSE)
-    board.digital[27].write(1)
-    time.sleep(PAUSE)
-    board.digital[27].write(0)
-    time.sleep(PAUSE)
 
 def do_shift(shift: str) -> None: # phase shifter command function
     for i in range(0,len(shift)):
