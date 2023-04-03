11eb5298f983a648de608f8af6d21a3b3702ffe2
Lucas Gray <lucascgray@protonmail.com>
2023-04-02 19:20:59 -0700

    generalized phase shift angle acquiring function; added test constants

--- antenna.py
+++ antenna.py
@@ -1,9 +1,15 @@
-import pyfirmata
+from typing import Tuple
 import pandas as pd
 import time
+import pyfirmata
 
-PAUSE = 0.01 # 'clock' speed
-ZERO = 0
+PAUSE = 0.005 # 'clock' period
+ZERO = (0, 90)
+UP = (0, 60)
+LEFT = (90, 83)
+RIGHT = (270, 82.8)
+UP_L = (45, 45)
+UP_R = (315, 45)
 
 board = pyfirmata.ArduinoMega('/dev/cu.usbmodem101')
 data = pd.read_csv('lookangles.csv')
@@ -11,11 +17,14 @@ data = pd.read_csv('lookangles.csv')
 def bits(angle: float) -> str:
     return bin(int(angle / 22.5) * 4)[2:].zfill(6)
 
-def get_angles(index: int) -> str: # select wanted EL and AZ set
-    cmd  = bits(data.J4[index])
-    cmd += bits(data.J3[index])
-    cmd += bits(data.J2[index])
-    cmd += bits(data.J1[index])
+def get_angles(point: Tuple[float, float]) -> str: # select wanted EL and AZ
+    for i in range(0, len(data)):            # phase shift set
+        if data.AZ[i] == point[0] and data.EL[i] == point[1]:
+            cmd  = bits(data.J4[i])
+            cmd += bits(data.J3[i])
+            cmd += bits(data.J2[i])
+            cmd += bits(data.J1[i])
+            break
     return cmd
 
 def do_shift(shift: str) -> None: # phase shifter command function
