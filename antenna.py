23b40b84dd0d85f362c402b8a2cbb5f10b1c97a1
Lucas Gray <lucascgray@protonmail.com>
2023-04-04 19:32:26 -0700

    quickfix: remove animation module

--- antenna.py
+++ antenna.py
@@ -2,7 +2,6 @@ from typing import Tuple
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import time
 import subprocess as sp
 import re
