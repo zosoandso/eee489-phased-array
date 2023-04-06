e3b8a27fd1bd9710444525478cf6e4fa434df3ba
Lucas Gray <lucascgray@protonmail.com>
2023-04-06 11:41:17 -0700

    lowered size of nearby search radius

--- search.py
+++ search.py
@@ -8,7 +8,7 @@ def dumb_search() -> None: # just a test demo don't use
         an.plot_beam(point)
 
 def quick_search() -> Tuple[float, float]:
-    power = -100
+    power = -150
     for i in range(0, len(an.data)):
         for angle in [0, 45, 90, 135, 180, 225, 270, 315]:
             if an.data.AZ[i] == angle:
@@ -23,11 +23,11 @@ def quick_search() -> Tuple[float, float]:
 
 def search_nearby_az(point: Tuple[float, float]) -> Tuple[float, float]:
     power = -100
-    if point[0] + 20 > 360:
+    if point[0] + 15 > 360:
         for i in range(0, len(an.data)):
-            if ((an.data.AZ[i] < point[0] + 20 and
-                 an.data.AZ[i] > point[0] - 20) or
-                 (an.data.AZ[i] < (point[0] + 20) % 360 and
+            if ((an.data.AZ[i] < point[0] + 15 and
+                 an.data.AZ[i] > point[0] - 15) or
+                 (an.data.AZ[i] < (point[0] + 15) % 360 and
                   an.data.AZ[i] > 0)):
                 move = (an.data.AZ[i], an.data.EL[i])
                 an.do_shift(move)
@@ -35,11 +35,11 @@ def search_nearby_az(point: Tuple[float, float]) -> Tuple[float, float]:
                 if an.get_rssi_mac() > power:
                     power = an.get_rssi_mac()
                     best_move = move
-    elif point[0] - 20 < 0:
+    elif point[0] - 15 < 0:
         for i in range(0, len(an.data)):
-            if ((an.data.AZ[i] > point[0] - 20 and
-                 an.data.AZ[i] < point[0] + 20) or
-                 (an.data.AZ[i] > (point[0] - 20) % 360 and
+            if ((an.data.AZ[i] > point[0] - 15 and
+                 an.data.AZ[i] < point[0] + 15) or
+                 (an.data.AZ[i] > (point[0] - 15) % 360 and
                   an.data.AZ[i] < 360)):
                 move = (an.data.AZ[i], an.data.EL[i])
                 an.do_shift(move)
@@ -49,8 +49,8 @@ def search_nearby_az(point: Tuple[float, float]) -> Tuple[float, float]:
                     best_move = move
     else:
         for i in range(0, len(an.data)):
-            if (an.data.AZ[i] > point[0] - 20 and
-                 an.data.AZ[i] < point[0] + 20):
+            if (an.data.AZ[i] > point[0] - 15 and
+                 an.data.AZ[i] < point[0] + 15):
                 move = (an.data.AZ[i], an.data.EL[i])
                 an.do_shift(move)
                 an.plot_beam(move)
