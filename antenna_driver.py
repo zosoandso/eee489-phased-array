9b45b7e83b454b3e8c4c54946f8d8b8c56ce5690
Lucas Gray <lucascgray@protonmail.com>
2023-04-07 01:22:34 -0700

    standardization

--- antenna_driver.py
+++ antenna_driver.py
@@ -26,9 +26,9 @@ def bits(angle: float) -> str:
     return bin(int(angle / 22.5) * 4)[2:].zfill(6)
 
 def get_shift(point: Tuple[float, float]) -> str: # select wanted EL and AZ
-    newData = np.loadtxt('lookangles.csv',delimiter=',' ,skiprows=1)
-    phase = newData[np.nonzero(np.logical_and(newData[:,0]==point[0],
-                                              newData[:,1]==point[1]))][0,2:]
+    data = np.loadtxt('lookangles.csv',delimiter=',' ,skiprows=1)
+    phase = data[np.nonzero(np.logical_and(data[:,0]==point[0],
+                                              data[:,1]==point[1]))][0,2:]
     if phase.shape[0] > 0:
         shift  = bits(phase[0])
         shift += bits(phase[1])
