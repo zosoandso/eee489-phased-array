31211aad0ab69d78e97149cce6990c1673bce945
Lucas Gray <lucascgray@protonmail.com>
2023-04-06 18:02:25 -0700

    moved Travis' improved get_shift code into antenna.py and cleaned up do_shift by adding write

--- antenna.py
+++ antenna.py
@@ -8,41 +8,39 @@ import time
 import re
 
 PAUSE = 0.001  # 'clock' period
+SERIAL = 23
+CLOCK = 25
+ENABLE = 27
 
 #board = pyfirmata.ArduinoMega('dev/ttyACM0') # linux
-board = pyfirmata.ArduinoMega('/dev/cu.usbmodem1101') # mac
+board = pyfirmata.ArduinoMega('/dev/cu.usbmodem101') # mac
 data = pd.read_csv('lookangles.csv')
+
+def write(pin: int, logic: int) -> None:
+    board.digital[pin].write(logic)
+    time.sleep(PAUSE)
 
 def bits(angle: float) -> str:
     return bin(int(angle / 22.5) * 4)[2:].zfill(6)
 
 def get_shift(point: Tuple[float, float]) -> str: # select wanted EL and AZ
-    for i in range(0, len(data)):                 # phase shift set
-        if data.AZ[i] == point[0] and data.EL[i] == point[1]:
-            shift  = bits(data.J4[i])
-            shift += bits(data.J3[i])
-            shift += bits(data.J2[i])
-            shift += bits(data.J1[i])
-            break
+    newData = np.loadtxt('lookangles.csv',delimiter=',' ,skiprows=1)
+    phase = newData[np.nonzero(np.logical_and(newData[:,0]==point[0],
+                                              newData[:,1]==point[1]))][0,2:]
+    shift  = bits(phase[0])
+    shift += bits(phase[1])
+    shift += bits(phase[2])
+    shift += bits(phase[3])  
     return shift
 
 def do_shift(point: Tuple[float, float]) -> None: # phase shift command
     shift = get_shift(point)
     for i in range(0,len(shift)):
-        if shift[i] == '1':
-            board.digital[23].write(1)
-            time.sleep(PAUSE)
-        else:
-            board.digital[23].write(0)
-            time.sleep(PAUSE) 
-        board.digital[25].write(1)
-        time.sleep(PAUSE)
-        board.digital[25].write(0)
-        time.sleep(PAUSE)
-    board.digital[27].write(1)
-    time.sleep(PAUSE)
-    board.digital[27].write(0)
-    time.sleep(PAUSE)
+        write(SERIAL, 1) if shift[i] == '1' else write(SERIAL, 0)
+        write(CLOCK, 1)
+        write(CLOCK, 0)
+    write(ENABLE, 1)
+    write(ENABLE, 0)
 
 def get_rssi_linux() -> int:
     power = sp.check_output(['iwconfig', 'wlp6s0'])
