8dcdcad90d21c077d8d0dec5723970e0a7a2383d
Lucas Gray <lucascgray@protonmail.com>
2023-04-12 11:26:13 -0700

    updated main with help documentation

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import antenna_driver as an
 import search
+import Step_Track
 
 while True:
-    prompt = input('Enter antenna task: ')
-    if prompt == '':
+    prompt = input('Enter antenna task (help for list of commands): ')
+    if prompt == 'help':
+        print('connect: connects to specifed access point (AP)')
+        print('search: directs antenna to point at static AP')
+        print('track: tracks relative movement of AP and adjusts antenna')
+        print('exit: leaves the antenna control environment')
+    if prompt == 'exit':
         break
     elif prompt == 'connect':
         an.connect()
     elif prompt == 'search':
         point = search.quick_search()
-        point = search.search_nearby(point)
+        search.search_nearby(point)
+    elif prompt == 'track':
+        print()
     else:
-        print('Invalid entry: try again.')
+        print('Invalid entry try again.')
